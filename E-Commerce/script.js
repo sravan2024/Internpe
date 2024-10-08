f64b3bb55855a17a04679b87443049391a935219
sravan <157780767+sravan2024@users.noreply.github.com>
2024-10-08 13:07:48 +0530

    Add files via upload

---
+++ E-Commerce/script.js
@@ -0,0 +1,11 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    const buttons = document.querySelectorAll('.product button');
+
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            alert('Product added to cart!');
+        });
+    });
+});
